4d95e6e08ff9468cf248c281b27d5b566506f638
ADALISK\akoksin <akoksin@adalisk.com>
2022-07-17 15:52:27 +0400

    add more tests

--- Tests/TestCliParser.cpp
+++ Tests/TestCliParser.cpp
@@ -36,11 +36,65 @@ END_TEST
 
 BEGIN_TEST( CliParser, NothingToStack )
 
-std::vector<const char*> data = { "AstroCombiner", "-stack", "-output", "test.txt"};
+std::vector<const char*> data = {  "-stack", "-output", "test.txt"};
 CliParser parser( data.size(), const_cast< char** >( &data[0] ) );
 auto res = parser.Parse( true );
 EXPECT_EQ( 1, res.first );
 EXPECT_EQ( "Nothing to stack", res.second );
+
+END_TEST
+
+BEGIN_TEST( CliParser, StackOneFile )
+
+std::vector<const char*> data = { "-stack", GetPathToTestFile( "RAW/IMG_8899.CR2" ).data(), "-output", "test.txt"};
+CliParser parser( data.size(), const_cast< char** >( &data[0] ) );
+auto res = parser.Parse( true );
+EXPECT_EQ( 0, res.first );
+EXPECT_EQ( 1, parser._decoders.size() );
+
+END_TEST
+
+BEGIN_TEST( CliParser, StackTwoFiles )
+
+std::string fileNames = GetPathToTestFile( "RAW/MilkyWayCR2/IMG_8944.CR2" ) + ";" + GetPathToTestFile( "RAW/MilkyWayCR2/IMG_8945.CR2" );
+std::vector<const char*> data = { "-stack", fileNames.data(), "-output", "test.txt" };
+CliParser parser( data.size(), const_cast< char** >( &data[0] ) );
+auto res = parser.Parse( true );
+EXPECT_EQ( 0, res.first );
+EXPECT_EQ( 2, parser._decoders.size() );
+
+END_TEST
+
+BEGIN_TEST( CliParser, StackWrongDirectory )
+
+std::string dirName = GetPathToTestFile( "RAW/MilkyWayCR4/" );
+std::vector<const char*> data = { "-stack", "-dir", dirName.data(), "-output", "test.txt" };
+CliParser parser( data.size(), const_cast< char** >( &data[0] ) );
+auto res = parser.Parse( true );
+EXPECT_EQ( 1, res.first );
+EXPECT_EQ( "No such directory", res.second );
+
+END_TEST
+
+BEGIN_TEST( CliParser, StackDirectory )
+
+std::string dirName = GetPathToTestFile( "RAW/MilkyWayCR2/" );
+std::vector<const char*> data = { "-stack", "-dir", dirName.data(), "-output", "test.txt"};
+CliParser parser( data.size(), const_cast< char** >( &data[0] ) );
+auto res = parser.Parse( true );
+EXPECT_EQ( 0, res.first );
+EXPECT_EQ( 27, parser._decoders.size() );
+
+END_TEST
+
+BEGIN_TEST( CliParser, StackRange )
+
+std::string fileRange = GetPathToTestFile( "RAW/MilkyWayCR2/" ) + "IMG_8947~63.CR2";
+std::vector<const char*> data = { "-stack", fileRange.data(), "-output", "test.txt" };
+CliParser parser( data.size(), const_cast< char** >( &data[0] ) );
+auto res = parser.Parse( true );
+EXPECT_EQ( 0, res.first );
+EXPECT_EQ( 17, parser._decoders.size() );
 
 END_TEST
 
