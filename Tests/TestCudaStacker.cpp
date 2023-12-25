24320b37023e8fff7706b7a234acb94d5b21158f
ASTROCOMP\astro <akoksin@adalisk.com>
2023-12-25 11:19:43 +0400

    edit test

--- Tests/TestCudaStacker.cpp
+++ Tests/TestCudaStacker.cpp
@@ -12,10 +12,8 @@ BEGIN_SUITE( CudaStacker )
 BEGIN_TEST( TwoBitmaps )
 
 std::vector<Pipeline> pipelines;
-auto pRedBitmap = std::make_shared<Bitmap<PixelFormat::RGB24>>(10,10, MakeRGB24(255,0,0));
-auto pBlueBitmap = std::make_shared<Bitmap<PixelFormat::RGB24>>(10,10, MakeRGB24(0,0,255));
-pipelines.emplace_back( pRedBitmap );
-pipelines.emplace_back( pBlueBitmap );
+pipelines.emplace_back( std::make_shared<Bitmap<PixelFormat::RGB24>>( 10, 10, MakeRGB24( 254, 0, 0 ) ) );
+pipelines.emplace_back( std::make_shared<Bitmap<PixelFormat::RGB24>>( 10, 10, MakeRGB24( 0, 0, 254 ) ) );
 
 Stacker stacker( pipelines, StackMode::DarkOrFlat );
 cuda::Stacker cudaStacker( pipelines, StackMode::DarkOrFlat );
